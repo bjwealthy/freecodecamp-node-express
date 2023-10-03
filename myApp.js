78036da967dbaa4f25c7c550d9486c5cf2d857cb
mobolaji <bjwealthy@gmail.com>
2023-10-03 02:05:37 +0100

    use .env file

--- myApp.js
+++ myApp.js
@@ -9,7 +9,7 @@ console.log("Hello World")
 
 app.get('/', (req, res) => {
     //res.sendFile(__dirname +'/views/index.html')
-    res.send(process.env.MESSAGE_STYLE ? {"message": "HELLO JSON"} : {"message": "Hello json"})
+    console.log(process.env.MESSAGE_STYLE ? {"message": "HELLO JSON"} : {"message": "Hello json"})
 })
 
 app.use('/public', express.static(__dirname + '/public'))
